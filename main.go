47f67d1ae1ac6ece3d50344433f66c2093b17b12
tanryberdi.syhmyradow <tanryberdi.syhmyradow@rain.us>
2022-01-26 19:09:28 +0500

    rabbit-mq tutorial

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+// https://www.youtube.com/watch?v=pAXp6o-zWS4
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	fmt.Println("Go RabbitMQ Tutorial")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to out RabbitMQ Instance")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println(q)
+
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World"),
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println("Successfully Published Message to Queue")
+
+}
